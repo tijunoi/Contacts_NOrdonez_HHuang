ff3e09ec413ed7df24117a0d4e2ed67ecfbc9797
Nil Ordoñez <nilordonez.7@gmail.com>
2018-03-13 17:47:53 +0100

    FMDB imported and first UI Prototype

--- Contacts_NOrdonez_HHuang/ViewController.swift
+++ Contacts_NOrdonez_HHuang/ViewController.swift
@@ -7,12 +7,50 @@
 //
 
 import UIKit
+import Hero
 
 class ViewController: UIViewController {
 
+    let dataBaseFileName = "contacts.db"
+    var dataBasePath:String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpDataBase()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //Get documents device directory path and set to the dataBasePath property
+    func setUpDataBase(){
+        let fileManager = FileManager()
+        if let dirDocument = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first{
+            let dataBaseURL = dirDocument.appendingPathComponent(dataBaseFileName)
+            dataBasePath = dataBaseURL.path
+            
+            //Create if not exists the database (users.db) or get the reference in dataBAsePath
+            let userDB = FMDatabase(path: dataBasePath)
+            
+            let createStatement = """
+            CREATE TABLE IF NOT EXISTS CONTACTS (
+                ID INTEGER PRIMARY KEY,
+                NAME TEXT,
+                LASTNAME TEXT,
+                NUM1 TEXT,
+                NUM2 TEXT,
+                EMAIL TEXT,
+                ISFAV INTEGER)
+            """
+            
+            if userDB.open(){
+                if !userDB.executeStatements(createStatement){
+                    print(userDB.lastError().localizedDescription)
+                }
+                userDB.close()
+            }else{
+                print(userDB.lastError().localizedDescription)
+            }
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Podfile
@@ -0,0 +1,12 @@
+project 'Contacts_NOrdonez_HHuang.xcodeproj'
+
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Contacts_NOrdonez_HHuang' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Contacts_NOrdonez_HHuang
+  pod 'Hero'
+end
